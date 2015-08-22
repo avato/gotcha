4a74671d5284b1fe5a95dfc2873515a9ea9dccd0
AJ <a@ava.to>
2015-08-22 11:38:22 +0200

    Modal form added

---
+++ includes/template/html-gc-modal.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Gotcha modal html
+*
+* @link              http://ava.to
+* @since             0.1
+* @package           Gotcha
+*
+*
+*/
+if ( ! defined( 'WPINC' ) ) die;
+
+?>
+
+
+
+
+
+<div class="gc-modalbox"></div>
+
+<div class="gc-modal_wrapper">
+
+	<div class="gc-login-wrapper">
+   		<?php echo wp_login_form(); ?> 
+   	</div>
+
+</div>
